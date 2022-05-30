5e4f11653b4155ac8dae64ff52fd4c68807f8269
herdeybayor <shereefdeen4real@gmail.com>
2022-05-30 10:52:43 +0100

    testing

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
+require("dotenv").config();
 const http = require("http");
 const app = require("./app");
 const logger = require("./utils/logger");
 const config = require("./utils/config");
 
 const server = http.createServer(app);
-
+logger.info("Hello", config.PORT);
+logger.info("Hi", process.env.PORT);
 server.listen(config.PORT, () => {
   logger.info(
     `Server running on port ${config.PORT}\nVisit http://localhost:${config.PORT}`
