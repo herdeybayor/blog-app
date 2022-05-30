b0e99d3fbd9e1c294ee7aeb32a453ad2008a14b6
herdeybayor <shereefdeen4real@gmail.com>
2022-05-30 11:00:40 +0100

    remove logs

--- index.js
+++ index.js
@@ -1,12 +1,10 @@
-require("dotenv").config();
 const http = require("http");
 const app = require("./app");
 const logger = require("./utils/logger");
 const config = require("./utils/config");
 
 const server = http.createServer(app);
-logger.info("Hello", config.PORT);
-logger.info("Hi", process.env.PORT);
+
 server.listen(config.PORT, () => {
   logger.info(
     `Server running on port ${config.PORT}\nVisit http://localhost:${config.PORT}`
