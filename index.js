db0a001160e4b877ac78b2c74373c6b846d0d7e7
herdeybayor <shereefdeen4real@gmail.com>
2022-05-30 10:38:32 +0100

    remove procfile

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-require("dotenv").config();
 const http = require("http");
 const app = require("./app");
 const logger = require("./utils/logger");
@@ -6,7 +5,7 @@ const config = require("./utils/config");
 
 const server = http.createServer(app);
 
-server.listen(process.env.PORT, () => {
+server.listen(config.PORT, () => {
   logger.info(
     `Server running on port ${config.PORT}\nVisit http://localhost:${config.PORT}`
   );
